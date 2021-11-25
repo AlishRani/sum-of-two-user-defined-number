cfdcc7d0d4248774c87e58e206c6f89cd1d73e18
Alish Rani <94797867+AlishRani@users.noreply.github.com>
2021-11-25 18:12:26 +0530

    Add files via upload

---
+++ sum of two user defined number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+     int a;
+     int b;
+     printf("enter the value of a\n");
+     scanf("%d",&a);
+     printf("enter the value of b\n");
+     scanf("%d",&b);
+     int c=a+b;
+     printf("sum of a and b%d",c);
+     return 0;
+}
